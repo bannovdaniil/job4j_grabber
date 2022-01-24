9168faa73723dd7940192fd25712098d9342971b
bannovdaniil <baddan@mail.ru>
2022-01-24 18:42:32 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,44 +4,72 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.Post;
 import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SqlRuParse {
+    private List<Post> posts;
+
     /**
-     * Слишком простой вариант. Лане не понравится. Но имеет место на существование.
-     * 1.
-     * String url = "https://www.sql.ru/forum/job-offers/" + (i == 0 ? "" : i);
-     * 2.
-     * Elements hrefs = doc.select(".sort_options").get(1).getElementsByTag("a");
-     * for (Element href : hrefs) {
-     * if (Integer.valueOf(i + 1).toString().equals(href.text())) {
-     * url = href.attr("href");
-     * break;
-     * }
-     * }
+     *
      */
     public static void main(String[] args) throws Exception {
         SqlRuParse sqlRuParse = new SqlRuParse();
+        sqlRuParse.posts = new ArrayList<>();
         String url = "https://www.sql.ru/forum/job-offers/";
         for (int i = 0; i < 5; i++) {
             System.out.println(System.lineSeparator() + "Page: " + i);
-            Document doc = sqlRuParse.parsePage(url + (i == 0 ? "" : i));
+            sqlRuParse.parsePage(url + (i == 0 ? "" : i));
             Thread.sleep(3000);
         }
     }
 
-    private Document parsePage(String url) throws Exception {
-        Document doc = Jsoup.connect(url).get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            Element parent = td.parent();
-            SqlRuDateTimeParser sqlRuDateTimeParser = new SqlRuDateTimeParser();
-            System.out.println(parent.child(5).text() + " => "
-                    + sqlRuDateTimeParser.parse(parent.child(5).text()));
+    /**
+     * Вытаскиваем Пост с работой
+     *
+     * @param postLink - ulr поста
+     * @return - тескс поста
+     */
+    private String getPost(String postLink) throws Exception {
+        Document doc = Jsoup.connect(postLink).get();
+        String text = "";
+        Elements textBody = doc.select(".msgBody");
+        if (textBody.size() > 1) {
+            text = textBody.get(1).text();
         }
-        return doc;
+        return text;
+    }
+
+    /**
+     * Вытаскиваем все линки на посты.
+     */
+    private void parsePage(String url) throws Exception {
+        Document doc = Jsoup.connect(url).get();
+        SqlRuDateTimeParser sqlRuDateTimeParser = new SqlRuDateTimeParser();
+        Elements rowTr = doc.getElementsByTag("tr");
+        int count = 0;
+        for (Element row : rowTr) {
+            Elements cols = row.getElementsByTag("td");
+            Elements dates = cols.select(".altCol");
+            if (!dates.isEmpty()) {
+                Elements postLinks = cols.select(".postslisttopic");
+                if (!postLinks.isEmpty() && !postLinks.get(0).toString().contains("closedTopic")) {
+                    String date = dates.get(1).text();
+                    Element href = postLinks.get(0).getElementsByTag("a").get(0);
+                    String postLink = href.attr("href");
+                    String postText = href.text();
+                    String postDescription = getPost(postLink);
+                    System.out.println(date + postLink + ": " + postText);
+                    System.out.println(postDescription);
+                    LocalDateTime postDate = sqlRuDateTimeParser.parse(date);
+                    posts.add(new Post(count++, postText, postLink, postDescription, postDate));
+                    Thread.sleep(2000);
+                }
+            }
+        }
     }
 }
